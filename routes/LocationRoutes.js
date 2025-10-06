439b553ba846f1e8a6b03575f91912b904ee3585
Bald-Duck294 <goolumoolu294@gmail.com>
2025-10-07 02:56:50 +0530

    "location update"

--- routes/LocationRoutes.js
+++ routes/LocationRoutes.js
@@ -5,10 +5,12 @@ import {
   createLocation,
   getZonesWithToilets,
   getNearbyLocations,
-  
+deleteLocationImage,
   getSearchToilet,
-  updateLocationById
+  updateLocationById ,
+  deleteLocationById
 } from "../controller/LocationsController.js";
+import { upload, processAndUploadImages } from "../middlewares/imageUpload.js"
 
 console.log('in get location rutes');
 const getLocationRoutes = express.Router();
@@ -16,15 +18,35 @@ const getLocationRoutes = express.Router();
 // getLocationRoutes.get("/getUsers", getUser);
 // getLocationRoutes.get('/getLocations' , getLocation);
 getLocationRoutes.get("/", getAllToilets);
-getLocationRoutes.post("/", createLocation);
+// getLocationRoutes.post("/", createLocation);
 
 getLocationRoutes.get("/zones", getZonesWithToilets);
 getLocationRoutes.get('/nearby', getNearbyLocations);
 getLocationRoutes.get("/:id", getToiletById);
 getLocationRoutes.get("/search", getSearchToilet);
-getLocationRoutes.post("/update/:id", updateLocationById);
+getLocationRoutes.delete("/:id/image", deleteLocationImage);
+// Add this route to your locations routes
+getLocationRoutes.delete('/:id', deleteLocationById);
 
 
+// getLocationRoutes.post("/update/:id", updateLocationById);
+
+// ✅ Routes with image upload support
+getLocationRoutes.post("/",
+  upload.fields([{ name: 'images', maxCount: 10 }]), // Support up to 10 images
+  processAndUploadImages([
+    { fieldName: 'images', folder: 'locations', maxCount: 10 }
+  ]),
+  createLocation
+);
+
+getLocationRoutes.post("/update/:id",
+  upload.fields([{ name: 'images', maxCount: 10 }]),
+  processAndUploadImages([
+    { fieldName: 'images', folder: 'locations', maxCount: 10 }
+  ]),
+  updateLocationById
+);
 
 // -------------- old routes ---------------
 
