eb12b98021f81eb7d0ec8b9fb9dee6031a20ecc4
Goolu Moolu <goolumoolu294@gmail.com>
2026-02-03 18:49:20 +0530

    feat: introduce dashboard API endpoints for counts, washroom scores, weekly performance, and top locations, including RBAC integration.

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,15 +1,26 @@
 // routes/dashboardRoutes.js
-import express from 'express';
+import express from "express";
 import {
-    getDashboardCounts,
-    getTopRatedLocations,
-    getTodaysActivities
+  getDashboardCounts,
+  getTopRatedLocations,
+  getTodaysActivities,
+  getWashroomScoresSummary,
+  getWeeklyCleanerPerformance,
 } from "../controller/dashboardController.js";
-import { verifyToken } from '../middlewares/authMiddleware.js';
+import { verifyToken } from "../middlewares/authMiddleware.js";
 const dashboardRoutes = express.Router();
 
-dashboardRoutes.get('/counts', verifyToken, getDashboardCounts);
-dashboardRoutes.get('/top-locations', verifyToken, getTopRatedLocations);
-dashboardRoutes.get('/activities', verifyToken, getTodaysActivities);
-
+dashboardRoutes.get("/counts", verifyToken, getDashboardCounts);
+dashboardRoutes.get("/top-locations", verifyToken, getTopRatedLocations);
+dashboardRoutes.get("/activities", verifyToken, getTodaysActivities);
+dashboardRoutes.get(
+  "/graph-washroom-scores",
+  verifyToken,
+  getWashroomScoresSummary,
+);
+dashboardRoutes.get(
+  "/graph-cleaner-performance",
+  verifyToken,
+  getWeeklyCleanerPerformance,
+);
 export default dashboardRoutes;
